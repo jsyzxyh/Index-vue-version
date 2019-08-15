174d58a0d143b6039712c2adb549d8e58c88269a
没有如果 <751619916@qq.com>
2019-08-15 23:24:16 +0800

    增加返回顶部按钮

--- src/views/FrontEnd.vue
+++ src/views/FrontEnd.vue
@@ -13,6 +13,21 @@
             <p>主要来源：自己总结以及网络</p>
           </div>
           <router-view class="main"></router-view>
+          <el-backtop :bottom="50">
+            <div
+              style="{
+                height: 100%;
+                width: 100%;
+                background-color: #f2f5f6;
+                box-shadow: 0 0 6px rgba(0,0,0, .12);
+                text-align: center;
+                line-height: 40px;
+                color: #1989fa;
+              }"
+            >
+              UP
+            </div>
+          </el-backtop>
         </el-main>
       </el-container>
     </el-container>
